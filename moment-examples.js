8f1bcb0673d1b7641335ec716a6a7b226be611be
Brian Smith <432a234@gmail.com>
2016-05-17 22:23:29 -0700

    Show timestamp on UI

--- moment-examples.js
+++ moment-examples.js
@@ -4,13 +4,32 @@ var now = moment();
 // console.log(now.format('')); 
 // console.log(now.format('X')); 
 // console.log(now.format('x')); 
-console.log(now.valueOf('')); 
+// console.log(now.valueOf('')); 
 
-var timestamp = 1463543184914;
+// var jun = moment("2014-06-01T12:00:00Z");
+// var localtime jun.tz('America/Los_Angeles').format('ha z');
 
-var timestampMoment = moment.utc(timestamp);
+// var timestamp = 1463543184914;
+// var timestampMoment = moment.utc(timestamp);
 
-console.log(timestampMoment.local().format('h:mm a'));
+// console.log(timestampMoment.local().format('h:mm a'));
 
 
 // console.log(now.format(' MMM Mo YYYY, h:mm a')); // 6:45 pm
+
+var timestamp = now.format(' MMM Mo YYYY, h:mm a');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
